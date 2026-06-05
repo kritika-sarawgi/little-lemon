4fcea4b97740a1c41ddc6edaed45582a1d087ac7
Kritika sarawgi <100989747+kritika-sarawgi@users.noreply.github.com>
2026-06-05 21:05:09 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', include('restaurant.urls')),
+
+    path(
+        'restaurant/',
+        include('restaurant.urls')
+    ),
+
+    path(
+        'auth/',
+        include('djoser.urls')
+    ),
+
+    path(
+        'auth/',
+        include('djoser.urls.authtoken')
+    ),
+]
